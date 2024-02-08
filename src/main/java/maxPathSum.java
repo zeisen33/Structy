e25da8c2e3967ff03b2c3ff96472ca77c1bdd27b
Zane <zeisen33@gmail.com>
2024-02-07 21:07:17 -0500

    treePath

--- src/main/java/maxPathSum.java
+++ src/main/java/maxPathSum.java
@@ -1,4 +1,6 @@
-
+// Write a method, maxPathSum, that takes in the root of a binary tree that contains number values. The method should return the maximum sum of any root to leaf path within the tree.
+//
+// You may assume that the input tree is non-empty.
 
 
 
