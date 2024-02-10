a0079996c24e106007205c5760b7aef74e363667
Zane <zeisen33@gmail.com>
2024-02-10 12:03:02 -0500

    Remove Node

--- src/main/java/pathFinder.java
+++ src/main/java/pathFinder.java
@@ -2,17 +2,19 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
-class Node<T> {
-    T val;
-    Node<T> left;
-    Node<T> right;
 
-    public Node(T val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
-}
+// Node class is named already inside maxPathSum so this errors.
+//class Node<T> {
+//    T val;
+//    Node<T> left;
+//    Node<T> right;
+//
+//    public Node(T val) {
+//        this.val = val;
+//        this.left = null;
+//        this.right = null;
+//    }
+//}
 
 class Source {
     public static <T> List<T> pathFinder(Node<T> root, T target) {
