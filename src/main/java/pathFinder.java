d6fa950126b8fc581ffa4a2bc73413d3e210b8fe
Zane <zeisen33@gmail.com>
2024-02-09 19:15:32 -0500

    pathFinder

---
+++ src/main/java/pathFinder.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Node<T> {
+    T val;
+    Node<T> left;
+    Node<T> right;
+
+    public Node(T val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Source {
+    public static <T> List<T> pathFinder(Node<T> root, T target) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == target) {
+            return List.of(root.val);
+        }
+
+        ArrayList<T> finalList = new ArrayList<>();
+
+        finalList.add(root.val);
+        finalList.addAll(pathFinder(root.left, target));
+        finalList.addAll(pathFinder(root.right, target));
+
+        return Collections.reverse(finalList);
+    }
+
+
