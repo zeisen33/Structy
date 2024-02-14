df38a4c60e2b06fe0ff395a62ac05f996844719a
Zane <zeisen33@gmail.com>
2024-02-14 07:47:26 -0500

    HowHigh

---
+++ src/main/java/howHigh.java
@@ -0,0 +1,13 @@
+class Source {
+    public static <T> int howHigh(Node<T> root) {
+        if (root == null) {
+            return -1;
+        }
+
+        int leftPath;
+        int rightPath;
+        leftPath = 1 + howHigh(root.left);
+        rightPath = 1 + howHigh(root.right);
+        return Math.max(leftPath, rightPath);
+    }
+}
