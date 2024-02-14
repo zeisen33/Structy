fdb6356cffaebba6f4e52fe23ff7c6a4a91bd869
Zane <zeisen33@gmail.com>
2024-02-14 07:35:08 -0500

    treeCount

---
+++ src/main/java/treeCount.java
@@ -0,0 +1,28 @@
+import java.util.ArrayDeque;
+
+class Source {
+        public static <T> int treeValueCount(Node<T> root, T target) {
+            if (root == null) {
+                return 0;
+            }
+            int count = 0;
+
+            ArrayDeque<Node<T>> queue = new ArrayDeque<Node<T>>();
+            queue.push(root);
+
+            while (!queue.isEmpty()) {
+                Node<T> current = queue.pop();
+                if (current.val == target) {
+                    count++;
+                }
+                if (current.left != null) {
+                    queue.push(current.left);
+                }
+                if (current.right != null) {
+                    queue.push(current.right);
+                }
+            }
+
+            return count;
+        }
+}
