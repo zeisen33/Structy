f0e7d631a3993906f10206fb259f9cc8fae577bb
Zane <zeisen33@gmail.com>
2024-02-16 07:56:34 -0500

    pathFinder

--- src/main/java/pathFinder.java
+++ src/main/java/pathFinder.java
@@ -2,38 +2,59 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
-// Node class is named already inside maxPathSum so this errors.
-//class Node<T> {
-//    T val;
-//    Node<T> left;
-//    Node<T> right;
-//
-//    public Node(T val) {
-//        this.val = val;
-//        this.left = null;
-//        this.right = null;
-//    }
-//}
+class Node<T> {
+    T val;
+    Node<T> left;
+    Node<T> right;
+
+    public Node(T val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
 
 class Source {
     public static <T> List<T> pathFinder(Node<T> root, T target) {
+        List<T> path = pathFinderRecursive(root, target);
+        if (path == null) {
+            return null;
+        } else {
+            Collections.reverse(path);
+            return path;
+        }
+    }
+
+    public static <T> List<T> pathFinderRecursive(Node<T> root, T target) {
         if (root == null) {
             return null;
         }
 
+
         if (root.val == target) {
-            return List.of(root.val);
+            ArrayList<T> list = new ArrayList<>();
+            list.add(root.val);
+            return list;
         }
 
-        List<T> finalList = new ArrayList<>();
+        List<T> leftPath = pathFinderRecursive(root.left, target);
+        if (leftPath != null) {
+            leftPath.add(root.val);
+            return leftPath;
+        }
+        List<T> rightPath = pathFinderRecursive(root.right, target);
+        if (rightPath != null) {
+            rightPath.add(root.val);
+            return rightPath;
+        }
 
-        finalList.add(root.val);
-        finalList.addAll(pathFinder(root.left, target));
-        finalList.addAll(pathFinder(root.right, target));
+        return null;
+    }
 
-
-        // Why doesn't this work?
-        return Collections.reverse(finalList);
+    public static void run() {
+        // this function behaves as `main()` for the 'run' command
+        // you may sandbox in this function , but should not remove it
     }
 }
 
