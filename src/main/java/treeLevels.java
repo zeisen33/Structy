87e3715c48630254f7fed4553ccfec6d7b3b0448
Zane <zeisen33@gmail.com>
2024-02-16 10:24:56 -0500

    treeLevels

---
+++ src/main/java/treeLevels.java
@@ -0,0 +1,36 @@
+class Source {
+    public static <T> List<List<T>> treeLevels(Node<T> root) {
+        if (root == null) {
+            return List.of();
+        }
+        List<List<T>> finalList = new ArrayList<>();
+        ArrayDeque<SimpleEntry<Node<T>, Integer>> queue = new ArrayDeque<>();
+        queue.add(new SimpleEntry<Node<T>, Integer>(root, 0));
+        int currLevel = 0;
+        List<T> subList = new ArrayList<>();
+
+
+        while (!queue.isEmpty()) {
+            SimpleEntry<Node<T>, Integer> entry = queue.remove();
+            Node<T> node = entry.getKey();
+            int level = entry.getValue();
+            if (level == currLevel) {
+                subList.add(node.val);
+            } else {
+                finalList.add(subList);
+                currLevel += 1;
+                subList = new ArrayList<>();
+                subList.add(node.val);
+            }
+            if (node.left != null) {
+                queue.add(new SimpleEntry<Node<T>, Integer>(node.left, currLevel + 1));
+            }
+            if (node.right != null) {
+                queue.add(new SimpleEntry<Node<T>, Integer>(node.right, currLevel + 1));
+            }
+        }
+
+        finalList.add(subList);
+
+        return finalList;
+    }
