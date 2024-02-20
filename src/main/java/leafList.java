c1f77216851c3c715512270edf6c24c63d70ef55
Zane <zeisen33@gmail.com>
2024-02-20 09:04:21 -0500

    leafList

---
+++ src/main/java/leafList.java
@@ -0,0 +1,18 @@
+class Source {
+    public static <T> List<T> leafList(Node<T> root) {
+        if (root == null) {
+            return List.of();
+        }
+
+        if (root.left == null && root.right == null) {
+            return List.of(root.val);
+        }
+
+        List<T> leftPath = leafList(root.left);
+        List<T> rightPath = leafList(root.right);
+        List<T> finalList = new ArrayList<>();
+        finalList.addAll(leftPath);
+        finalList.addAll(rightPath);
+
+        return finalList;
+    }
