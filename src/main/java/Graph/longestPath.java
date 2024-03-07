657397e1d2d8d4ba3faf2cfd2614094fb036abcc
Zane <zeisen33@gmail.com>
2024-03-07 00:03:16 -0500

    Memoization

--- src/main/java/Graph/longestPath.java
+++ src/main/java/Graph/longestPath.java
@@ -2,12 +2,14 @@ package Graph;
 
 import java.util.Map;
 import java.util.List;
+import java.util.HashMap;
 
 class Source {
     public static int longestPath(Map<String, List<String>> graph) {
+        HashMap<String, Integer> distance = new HashMap<>();
         int max = 0;
         for (String node : graph.keySet()) {
-            int count = longestPathFrom(graph, node);
+            int count = longestPathFrom(graph, node, distance);
             if (count > max) {
                 max = count;
             }
@@ -15,14 +17,18 @@ class Source {
         return max;
     }
 
-    public static int longestPathFrom(Map<String, List<String>> graph, String startNode) {
+    public static int longestPathFrom(Map<String, List<String>> graph, String startNode, HashMap<String, Integer> distance) {
+        if (distance.containsKey(startNode)) {
+            return distance.get(startNode);
+        }
         List<String> neighbors = graph.get(startNode);
         if (neighbors.size() == 0) {
             return 0;
         }
         int max = 0;
         for (String neighbor : neighbors) {
-            int result = longestPathFrom(graph, neighbor);
+            int result = longestPathFrom(graph, neighbor, distance);
+            distance.put(neighbor, result);
             if (result > max) {
                 max = result;
             }
