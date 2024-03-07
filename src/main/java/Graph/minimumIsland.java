6b220a415a0e110be0b9ad163fbaa30d3a6ac891
Zane <zeisen33@gmail.com>
2024-03-06 19:12:15 -0500

    minimumIsland

--- src/main/java/Graph/minimumIsland.java
+++ src/main/java/Graph/minimumIsland.java
@@ -1,3 +1,40 @@
 package Graph;
+import java.util.List;
+import java.util.HashSet;
+import java.util.AbstractMap.SimpleEntry;
 
-import java.util.list
+
+class Source {
+    public static int minimumIsland(List<List<String>> grid) {
+        double minCount = Double.POSITIVE_INFINITY;
+        HashSet<SimpleEntry<Integer, Integer>> visited = new HashSet<>();
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid.get(0).size(); j++) {
+                int subCount = explore(i, j, visited, grid);
+                if (subCount < minCount && subCount > 0) {
+                    minCount = subCount;
+                }
+            }
+        }
+
+        return (int)minCount;
+    }
+
+    public static int explore(int row, int col, HashSet<SimpleEntry<Integer, Integer>> visited, List<List<String>> grid) {
+        if (row < 0 || row >= grid.size()) {
+            return 0;
+        }
+        if (col < 0 || col >= grid.get(0).size()) {
+            return 0;
+        }
+        SimpleEntry<Integer, Integer> pos = new SimpleEntry<>(row, col);
+        if (visited.contains(pos)) {
+            return 0;
+        }
+        visited.add(pos);
+        if (grid.get(row).get(col) == "W") {
+            return 0;
+        }
+        return 1 + explore(row - 1, col, visited, grid) + explore(row + 1, col, visited, grid) + explore(row, col - 1, visited, grid) + explore(row, col + 1, visited, grid);
+    }
