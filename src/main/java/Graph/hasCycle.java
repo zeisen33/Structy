eee28551b3ea2b48aea2c72a9858f3c840f7df52
Zane <zeisen33@gmail.com>
2024-03-08 22:32:22 -0500

    hasCycle

--- src/main/java/Graph/hasCycle.java
+++ src/main/java/Graph/hasCycle.java
@@ -1,4 +1,39 @@
 package Graph;
 
-public class hasCycle {
-}
+import java.util.Map;
+import java.util.List;
+import java.util.HashSet;
+
+class Source {
+    public static boolean hasCycle(Map<String, List<String>> graph) {
+        HashSet<String> inProgress = new HashSet<>();
+        HashSet<String> visited = new HashSet<>();
+
+        for (String node : graph.keySet()) {
+            if (explore(node, inProgress, visited, graph)) {
+                return true;
+            }
+            visited.addAll(inProgress);
+            inProgress.clear();
+        }
+        return false;
+    }
+
+    public static boolean explore(String node, HashSet<String> inProgress, HashSet<String> visited, Map<String, List<String>> graph) {
+        if (inProgress.contains(node)) {
+            return true;
+        }
+        if (visited.contains(node)) {
+            return false;
+        }
+        if (graph.get(node).size() == 0) {
+            return false;
+        }
+        inProgress.add(node);
+        for (String neighbor : graph.get(node)) {
+            if (explore(neighbor, inProgress, visited, graph)) {
+                return true;
+            }
+        }
+        return false;
+    }
