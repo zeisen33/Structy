8a11369d75bbdf20e82a90de7ad2f6969c345112
Zane <zeisen33@gmail.com>
2024-03-17 20:46:39 -0400

    ArrayStepper

--- src/main/java/DynamicProgramming/arrayStepper.java
+++ src/main/java/DynamicProgramming/arrayStepper.java
@@ -1,2 +1,33 @@
 package DynamicProgramming;
 
+import java.util.List;
+import java.util.Stack;
+import java.util.HashSet;
+
+
+class Source {
+    public static boolean arrayStepper(List<Integer> nums) {
+        Stack<Integer> stack = new Stack<>();
+        HashSet<Integer> memo = new HashSet<>();
+        int lastIdx = nums.size() - 1;
+        stack.push(0);
+
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            System.out.println(current);
+            if (current == lastIdx) {
+                return true;
+            }
+            if (memo.contains(current)) {
+                continue;
+            }
+            if (current > lastIdx) {
+                continue;
+            }
+            for (int i = 1; i <= nums.get(current); i++) {
+                stack.push(current + i);
+            }
+            memo.add(current);
+        }
+        return false;
+    }
