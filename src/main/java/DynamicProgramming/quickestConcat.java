6a93a975eb9aab222a798a2b30f69341ca7af8fe
Zane <zeisen33@gmail.com>
2024-03-25 17:47:37 -0400

    My attempt quickestConcat

--- src/main/java/DynamicProgramming/quickestConcat.java
+++ src/main/java/DynamicProgramming/quickestConcat.java
@@ -1,7 +1,30 @@
 package DynamicProgramming;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.HashSet;
+import java.util.HashMap;
 
-// Similar to can Concat
+class Source {
+    public static int quickestConcat(String s, List<String> words) {
+        return quickestConcat(s, words, new HashMap<String, Integer>());
+    }
+    public static int quickestConcat(String s, List<String> words, HashMap<String, Integer> memo) {
+        System.out.println(s);
+        if (s.length() == 0) {
+            return 0;
+        }
+        if (memo.containsKey(s)) {
+            return memo.get(s);
+        }
+        int min = Integer.MAX_VALUE;
+        for (String word : words) {
+            if (s.startsWith(word)) {
+                int res = 1 + quickestConcat(s.substring(word.length()), words, memo);
+                System.out.println(res);
+                if (res < min) {
+                    min = res;
+                }
+            }
+        }
+        memo.put(s, min);
+        return min;
+    }
