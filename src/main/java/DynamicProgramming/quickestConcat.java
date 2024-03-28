884c7ee8d2c5e34e154f6cd054e32b5f0dba3176
Zane <zeisen33@gmail.com>
2024-03-27 20:25:17 -0400

    Quickest concat fix

--- src/main/java/DynamicProgramming/quickestConcat.java
+++ src/main/java/DynamicProgramming/quickestConcat.java
@@ -5,7 +5,15 @@ import java.util.HashMap;
 
 class Source {
     public static int quickestConcat(String s, List<String> words) {
-        return quickestConcat(s, words, new HashMap<String, Integer>());
+        // System.out.println("--- " + Integer.MAX_VALUE);
+        int res = quickestConcat(s, words, new HashMap<String, Integer>());
+        // want to use infinity. Should use Double.POSITIVE_INFITY but this works too.
+        // when we add 1 to Max_Value later, we don't want integer overflow, so we subtract 1 in preparation to add 1 without overflowing. Next time much easier to use Double.POSITIVE_INFITY which is actual infinity.
+        if (res == Integer.MAX_VALUE - 1) {
+            return -1;
+        } else {
+            return res;
+        }
     }
     public static int quickestConcat(String s, List<String> words, HashMap<String, Integer> memo) {
         System.out.println(s);
@@ -15,14 +23,17 @@ class Source {
         if (memo.containsKey(s)) {
             return memo.get(s);
         }
-        int min = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE - 1;
         for (String word : words) {
             if (s.startsWith(word)) {
-                int res = 1 + quickestConcat(s.substring(word.length()), words, memo);
-                System.out.println(res);
+                String subs = s.substring(word.length());
+                System.out.println("subs" + subs + "\n");
+                int res = 1 + quickestConcat(subs, words, memo);
+                System.out.printf("res: %s%n", res);
                 if (res < min) {
                     min = res;
                 }
+                System.out.printf("min: %s%n", min);
             }
         }
         memo.put(s, min);
