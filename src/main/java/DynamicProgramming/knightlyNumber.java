fe0d56ccf4561e27629e1e67fc72bd7d522c5f10
Zane <zeisen33@gmail.com>
2024-03-28 16:38:38 -0400

    Knightly Number Working

--- src/main/java/DynamicProgramming/knightlyNumber.java
+++ src/main/java/DynamicProgramming/knightlyNumber.java
@@ -4,10 +4,44 @@ import java.util.ArrayDeque;
 import java.util.List;
 import java.util.HashMap;
 
+
+
 class Source {
+    // Works recursively bc able to memoize since you run the same calculation multiple times.
+    public static int knightlyNumber(int n, int m, int kr, int kc, int pr, int pc)  {
+
+        return knightlyNumber(n, m, kr, kc, pr, pc, new HashMap<List<Integer>, Integer>());
+    }
+
+    public static int knightlyNumber(int n, int m, int kr, int kc, int pr, int pc, HashMap<List<Integer>, Integer> memo) {
+        System.out.println(kr + ", " + kc);
+        if (m == 0 && kr == pr && kc == pc) {
+            return 1;
+        }
+        if (m < 0) {
+            return 0;
+        }
+        if (kr < 0 || kr >= n || kc < 0 || kc >= n) {
+            return 0;
+        }
+        if (memo.containsKey(List.of(kr, kc, m))) {
+            return memo.get(List.of(kr, kc, m));
+        }
+
+        List<List<Integer>> moves = List.of(List.of(1,2), List.of(1, -2), List.of(2, 1), List.of(2,-1), List.of(-1, 2), List.of(-1, -2), List.of(-2, 1), List.of(-2, -1));
+
+        int sum = 0;
+        for (List<Integer> move : moves) {
+            int nr = kr + move.get(0);
+            int nc = kc + move.get(1);
+            sum += knightlyNumber(n, m - 1, nr, nc, pr, pc, memo);
+        }
+        memo.put(List.of(kr, kc, m), sum);
+        return sum;
+    }
 
 
-    // Times out because there's no way to memoize this problem with BFS
+    // Times out because there's no way to memoize this problem with BFS as you dont ever repeat calculations
     public static int knightlyNumber(int n, int m, int kr, int kc, int pr, int pc)  {
         ArrayDeque<List<Integer>> queue = new ArrayDeque<>();
         queue.add(List.of(kr, kc, 0));
