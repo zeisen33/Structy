a7d98de321b0233cf91f75eb07e708fa7b157c23
Zane <zeisen33@gmail.com>
2024-04-18 15:58:36 -0400

    Lowest Common Ancestor in O(n)

--- src/main/java/MixedRecall/lowestCommonAncestor.java
+++ src/main/java/MixedRecall/lowestCommonAncestor.java
@@ -1,5 +1,59 @@
 package MixedRecall;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Node<T> {
+    T val;
+    Node<T> left;
+    Node<T> right;
+
+    public Node(T val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Source {
+    public static String lowestCommonAncestor(Node<String> root, String val1, String val2) {
+        ArrayList<String> path1 = getPath(root, val1);
+        ArrayList<String> path2 = getPath(root, val2);
+        System.out.println(path1);
+        System.out.println(path2);
+
+        for (int i = 0; i < path1.size(); i++) {
+            if (path2.contains(path1.get(i))) {
+                return path1.get(i);
+            }
+        }
+        return root.val;
+    }
+
+    public static ArrayList<String> getPath(Node<String> root, String val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            ArrayList<String> path = new ArrayList<>();
+            path.add(val);
+            return path;
+        }
+
+        ArrayList<String> leftPath = getPath(root.left, val);
+        if (leftPath != null) {
+            leftPath.add(root.val);
+            return leftPath;
+        }
+        ArrayList<String> rightPath = getPath(root.right, val);
+        if (rightPath != null) {
+            rightPath.add(root.val);
+            return rightPath;
+        }
+        return null;
+    }
 class Source {
     public static String lowestCommonAncestor(Node<String> root, String val1, String val2) {
         if (dfsContains(root.left, val1) && dfsContains(root.left, val2)) {
