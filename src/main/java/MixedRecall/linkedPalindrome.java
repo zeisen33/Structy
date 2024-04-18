e31b0a4e936a3796340906ee269003d8176211ef
Zane <zeisen33@gmail.com>
2024-04-17 22:38:18 -0400

    linked Palindrome

--- src/main/java/MixedRecall/linkedPalindrome.java
+++ src/main/java/MixedRecall/linkedPalindrome.java
@@ -1,2 +1,36 @@
 package MixedRecall;
 
+import java.util.Stack;
+
+class Node<T> {
+    T val;
+    Node<T> next;
+
+    public Node(T val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Source {
+    public static <T> boolean linkedPalindrome(Node<T> head) {
+        Stack<Node<T>> stack = new Stack<>();
+        Node<T> curr = head;
+
+        while (curr != null) {
+            stack.push(curr);
+            curr = curr.next;
+        }
+
+        System.out.println(stack);
+
+        curr = head;
+        while (!stack.isEmpty()) {
+            if (curr != stack.pop()) {
+                return false;
+            }
+            curr = curr.next;
+        }
+
+        return true;
+    }
