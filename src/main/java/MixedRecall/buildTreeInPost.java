31bae04cb65382f03859d03290f93af672d2e0bc
Zane <zeisen33@gmail.com>
2024-04-30 23:08:08 -0400

    Build Tree in Post

--- src/main/java/MixedRecall/buildTreeInPost.java
+++ src/main/java/MixedRecall/buildTreeInPost.java
@@ -1,4 +1,22 @@
 package MixedRecall;
 
-public class buildTreeInPost {
-}
+class Source {
+    public static Node<String> buildTreeInPost(List<String> inOrder, List<String> postOrder) {
+        if (inOrder.size() == 0) {
+            return null;
+        }
+        if (inOrder.size() == 1 && postOrder.size() == 1) {
+            Node<String> node = new Node<>(inOrder.get(0));
+            return node;
+        }
+
+        String rootVal = postOrder.get(postOrder.size() - 1);
+        int midIdx = inOrder.indexOf(rootVal);
+
+        Node<String> left = buildTreeInPost(inOrder.subList(0, midIdx), postOrder.subList(0, midIdx));
+        Node<String> right = buildTreeInPost(inOrder.subList(midIdx + 1, inOrder.size()), postOrder.subList(midIdx, inOrder.size() - 1));
+        Node<String> root = new Node<>(rootVal);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
