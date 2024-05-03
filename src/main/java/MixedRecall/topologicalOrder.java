42a2ff4220f168669b878869c68dd209970070b7
Zane <zeisen33@gmail.com>
2024-05-03 01:12:39 -0400

    Topological Order

--- src/main/java/MixedRecall/topologicalOrder.java
+++ src/main/java/MixedRecall/topologicalOrder.java
@@ -1,5 +1,6 @@
 package MixedRecall;
 
+// Times out
 class Source {
     public static List<String> topologicalOrder(Map<String, List<String>> graph) {
         List<String> allNodes = new ArrayList<>();
@@ -42,3 +43,43 @@ class Source {
 
         return output;
     }
+
+    // Works
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+
+    class Source {
+        public static List<String> topologicalOrder(Map<String, List<String>> graph) {
+            HashMap<String, Integer> numParents = new HashMap<>();
+            for (String node : graph.keySet()) {
+                numParents.put(node, 0);
+            }
+
+            for (List<String> neighborList : graph.values()) {
+                for (String neighbor : neighborList) {
+                    numParents.put(neighbor, numParents.get(neighbor) + 1);
+                }
+            }
+
+            List<String> output = new ArrayList<>();
+
+            while (numParents.values().contains(0)) {
+                for (String node : numParents.keySet()) {
+                    if (numParents.get(node) == 0) {
+                        numParents.put(node, -1);
+                        output.add(node);
+                        for (String neighbor : graph.get(node)) {
+                            numParents.put(neighbor, numParents.get(neighbor) - 1);
+                        }
+                    }
+                }
+            }
+
+            // System.out.println(numParents);
+
+
+            return output;
+        }
