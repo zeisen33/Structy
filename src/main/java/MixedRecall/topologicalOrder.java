72710aa772b9c1ec624a8e4b783f6986b03f3300
Zane <zeisen33@gmail.com>
2024-05-03 20:29:21 -0400

    Comments

--- src/main/java/MixedRecall/topologicalOrder.java
+++ src/main/java/MixedRecall/topologicalOrder.java
@@ -1,48 +1,48 @@
 package MixedRecall;
 
 // Times out
-class Source {
-    public static List<String> topologicalOrder(Map<String, List<String>> graph) {
-        List<String> allNodes = new ArrayList<>();
-        String nextNode = null;
-        List<String> output = new ArrayList<>();
-        System.out.println(graph);
-        if (graph == null) {
-            return List.of();
-        }
-
-        for (List<String> neighborsList : graph.values()) {
-            for (String neighbor : neighborsList) {
-                allNodes.add(neighbor);
-            }
-        }
-
-        for (String node : graph.keySet()) {
-            if (!allNodes.contains(node)) {
-                nextNode = node;
-                break;
-            }
-        }
-
-        output.add(nextNode);
-
-        Map<String, List<String>> nextGraph = new HashMap<>();
-
-        for (Map.Entry<String, List<String>> entry : graph.entrySet()) {
-            if (entry.getKey() == nextNode) {
-                continue;
-            } else {
-                nextGraph.put(entry.getKey(), entry.getValue());
-            }
-        }
-
-
-        output.addAll(topologicalOrder(nextGraph));
-
-
-
-        return output;
-    }
+//class Source {
+//    public static List<String> topologicalOrder(Map<String, List<String>> graph) {
+//        List<String> allNodes = new ArrayList<>();
+//        String nextNode = null;
+//        List<String> output = new ArrayList<>();
+//        System.out.println(graph);
+//        if (graph == null) {
+//            return List.of();
+//        }
+//
+//        for (List<String> neighborsList : graph.values()) {
+//            for (String neighbor : neighborsList) {
+//                allNodes.add(neighbor);
+//            }
+//        }
+//
+//        for (String node : graph.keySet()) {
+//            if (!allNodes.contains(node)) {
+//                nextNode = node;
+//                break;
+//            }
+//        }
+//
+//        output.add(nextNode);
+//
+//        Map<String, List<String>> nextGraph = new HashMap<>();
+//
+//        for (Map.Entry<String, List<String>> entry : graph.entrySet()) {
+//            if (entry.getKey() == nextNode) {
+//                continue;
+//            } else {
+//                nextGraph.put(entry.getKey(), entry.getValue());
+//            }
+//        }
+//
+//
+//        output.addAll(topologicalOrder(nextGraph));
+//
+//
+//
+//        return output;
+//    }
 
     // Works
 import java.util.List;
